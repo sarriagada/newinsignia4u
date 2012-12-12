3e729947623a733be6a2687e1e0fc6582964f68f
insignia <army@insignia.(none)>
2012-12-12 16:05:58 -0300

    [initial commit]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Insignia::Application.config.secret_token = 'f51299ccaf9b4f0b784f791632f5e42b268b2b2523ccc11e9e516d39b1fab92eafe2db1c973b39e2deb4e8901921dbadaae9413f095e09c6a0b057019da3461c'
