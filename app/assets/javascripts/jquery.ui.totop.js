8aa3e9dda25a150c6c300915921b20efb8be9988
Sergio Sebastian Arriagada <sergio@insignia4u.com>
2013-01-23 12:00:07 -0300

    [modified toTop style]

--- app/assets/javascripts/jquery.ui.totop.js
+++ app/assets/javascripts/jquery.ui.totop.js
@@ -8,18 +8,17 @@
 	$.fn.UItoTop = function(options) {
 
  		var defaults = {
-    			text: 'To Top',
     			min: 200,
     			inDelay:600,
     			outDelay:400,
-      			containerID: 'toTop',
+      		containerID: 'toTop',
     			containerHoverID: 'toTopHover',
     			scrollSpeed: 1200,
     			easingType: 'linear'
- 		    },
-            settings = $.extend(defaults, options),
-            containerIDhash = '#' + settings.containerID,
-            containerHoverIDHash = '#'+settings.containerHoverID;
+ 		},
+    settings = $.extend(defaults, options),
+    containerIDhash = '#' + settings.containerID,
+    containerHoverIDHash = '#'+settings.containerHoverID;
 		
 		$('body').append('<a href="#" id="'+settings.containerID+'">'+settings.text+'</a>');
 		$(containerIDhash).hide().on('click.UItoTop',function(){
